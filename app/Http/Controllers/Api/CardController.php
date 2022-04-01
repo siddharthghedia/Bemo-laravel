60321ee1f01bc807bded023e0aaa5ad99701883d
Siddharth Ghedia <siddharthghedia@gmail.com>
2022-04-01 17:20:47 +0530

    Fix

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -22,7 +22,6 @@ class CardController extends Controller
                 return $q->get();
             });
             $query->when($request->get('status') == 'null', function ($q) use($request){
-                var_dump("xxx"); exit;
                 return $q->withTrashed()->get();
             });
         }])->where('user_id', auth()->user()->id)->get();
