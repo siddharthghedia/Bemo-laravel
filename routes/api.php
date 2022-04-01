1a30b3a69f9ac0d1646bd1c173c3f5a29ca55888
Siddharth Ghedia <siddharthghedia@gmail.com>
2022-04-01 14:27:58 +0530

    storage path added

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,7 @@ Route::get('/create-dump', function(){
         ->setDbName(env('DB_DATABASE'))
         ->setUserName(env('DB_USERNAME'))
         ->setPassword(env('DB_PASSWORD'))
+        ->setDumpBinaryPath(storage_path('/'))
         ->dumpToFile('dump.sql');
 });
 
