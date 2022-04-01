45fe3200c9909f3dc54634cd76ab93b96bdcd742
Siddharth Ghedia <siddharthghedia@gmail.com>
2022-04-01 16:29:13 +0530

    Fix

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,6 @@ Route::get('/create-dump', function(){
         ->setDbName(env('DB_DATABASE'))
         ->setUserName(env('DB_USERNAME'))
         ->setPassword(env('DB_PASSWORD'))
-        ->doNotUseColumnStatistics()
         ->setDumpBinaryPath(storage_path('mysql-backup'))
         ->dumpToFile('dump.sql');
 });
